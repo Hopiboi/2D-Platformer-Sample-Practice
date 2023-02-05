cd04f65262c9d7f60a53fd114e8d31b4e6700929
Hopiboi <89178015+Hopiboi@users.noreply.github.com>
2023-02-06 00:14:27 +0800

    2D Mockup

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -85,6 +85,7 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, grounded);
+
     }
 
 }
