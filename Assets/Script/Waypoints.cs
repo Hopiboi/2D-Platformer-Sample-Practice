d1cce4886ac347b2cb6c60f0eebf70458691415a
Hopiboi <89178015+Hopiboi@users.noreply.github.com>
2023-02-07 01:11:31 +0800

    2D Sample

--- Assets/Script/Waypoints.cs
+++ Assets/Script/Waypoints.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementPlatform : MonoBehaviour
+public class Waypoints : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] waypoints;
