7d1d7d608262121e6614bba45cd14ecadc9ca497
Hopiboi <89178015+Hopiboi@users.noreply.github.com>
2023-03-13 10:36:55 +0800

    Players

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    //Components
     private Rigidbody2D rg2D;
     private BoxCollider2D coll;
     private Animator anim;
@@ -12,10 +12,16 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private LayerMask grounded;
 
+    //Movement 
     [SerializeField] private float jumpPower = 5.4f;
+    [SerializeField] private float doubleJumpPower = 5.4f;
+    [SerializeField] private float jumpRelease = 2f;
     [SerializeField] private float moveSpeed = 7f;
     private float dirX = 0f;
+    private bool doubleJump;
 
+
+    //animation states
     private enum MovementState { idle, running, jump, fall }
 
     void Start()
@@ -38,10 +44,32 @@ public class PlayerMovement : MonoBehaviour
         dirX = Input.GetAxis("Horizontal");
         rg2D.velocity = new Vector2(dirX * moveSpeed, rg2D.velocity.y);
 
-        //jump
-        if (Input.GetButtonDown("Jump") && IsGrounded()) 
+
+        //Double Jump turning to false in the air
+
+
+        //Jump and Double Jump
+        if (Input.GetButtonDown("Jump")) 
         {
-            rg2D.velocity = new Vector2(rg2D.velocity.x, jumpPower);
+            if (IsGrounded())
+            {
+                rg2D.velocity = new Vector2(rg2D.velocity.x, jumpPower);
+
+                doubleJump = true;
+            }
+
+            else if (doubleJump)
+            {
+                rg2D.velocity = new Vector2(rg2D.velocity.x, doubleJumpPower);
+
+                doubleJump = false;
+            }
+        }
+
+        //Controlling the jump in short press or long press
+        if (Input.GetButtonUp("Jump"))
+        {
+            rg2D.velocity = new Vector2(rg2D.velocity.x, rg2D.velocity.y / jumpRelease);
         }
 
         AnimationMovement();
@@ -81,7 +109,9 @@ public class PlayerMovement : MonoBehaviour
 
         anim.SetInteger("state", (int)state);
     }
+    
 
+    //Checking if its Grounded
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, grounded);
